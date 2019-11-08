38a540be8e58293b096db2a554fe5cc9e0c8ab7b
Levente Kováts <22732484+tovask@users.noreply.github.com>
2019-11-08 10:35:43 +0100

    updating readme

--- optical_flow/optical_flow.ino
+++ optical_flow/optical_flow.ino
@@ -1,30 +1,5 @@
 /*
-
-  esp32:
-  https://github.com/espressif/esp32-camera
-
-  camera:
-  https://github.com/espressif/arduino-esp32/tree/master/libraries/ESP32/examples/Camera/CameraWebServer
-
-  optical flow:
-  https://github.com/PX4/Flow/blob/master/src/modules/flow/flow.c
-  https://github.com/PX4/OpticalFlow/blob/master/src/px4flow.cpp
-
-  mavlink:
-  https://mavlink.io/en/mavgen_c/
-  https://github.com/mavlink/c_library_v2
-  https://github.com/mavlink/c_library_v2/blob/master/common/mavlink_msg_optical_flow.h
-  https://github.com/PX4/Flow/blob/master/src/modules/flow/main.c#L572
-
-  i2c:
-  https://github.com/PX4/Flow/blob/master/src/include/i2c_frame.h
-  https://github.com/eschnou/arduino-px4flow-i2c
-
-  TFmini Plus (std9040):
-  http://en.benewake.com/product/detail/5c345cd0e5b3a844c472329b.html
-  https://github.com/senegalo/TFMiniPlus
-
-
+  Main code, where the other component used from.
 */
 
 #include "src/esp32_cam/camera.h"
